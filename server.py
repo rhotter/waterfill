383901d3134eced0ef505e46e6847e05c5792282
Raffi Hotter <raphael.hotter@gmail.com>
2023-12-15 02:45:28 -0500

    Make modal app a web app

--- server.py
+++ server.py
@@ -1,55 +1,68 @@
-from flask import Flask, request, jsonify
-from dataclasses import dataclass
-from transformers import CLIPSegProcessor, CLIPSegForImageSegmentation
-import torch
-from PIL import Image
-import io
-import cv2
+from modal import Stub, Image, method, wsgi_app
 
-# Initialize Flask app
-app = Flask(__name__)
+image = (
+    Image.debian_slim()
+    .pip_install(
+        "Pillow",
+        "torch",
+        "transformers",
+        "timm",
+        "flask"
+    ))
 
-# Load the model and processor
-processor = CLIPSegProcessor.from_pretrained("CIDAS/clipseg-rd64-refined")
-model = CLIPSegForImageSegmentation.from_pretrained("CIDAS/clipseg-rd64-refined")
+stub = Stub("waternug", image=image)
 
-# Data class for the response
-@dataclass
-class SegmentationResponse:
-    contours: list
 
-def detect_contours(preds):
-    mask = preds > 0.1
-    contours, _ = cv2.findContours(mask.astype('uint8'), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours_list = [contour.tolist() for contour in contours]
-    return contours_list
+@stub.cls(gpu="T4")
+class Model:
+    def __enter__(self):
+        from transformers import CLIPSegProcessor, CLIPSegForImageSegmentation
+        # Load the model and processor
+        self.processor = CLIPSegProcessor.from_pretrained(
+            "CIDAS/clipseg-rd64-refined")
+        self.model = CLIPSegForImageSegmentation.from_pretrained(
+            "CIDAS/clipseg-rd64-refined")
 
-@app.route('/segment', methods=['POST'])
-def segment_image():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image provided'}), 400
-    
-    # Read the image file
-    image_file = request.files['image']
-    image = Image.open(io.BytesIO(image_file.read()))
+    @method()
+    def segment(self, image_bytes):
+        from io import BytesIO
+        from PIL import Image
+        import torch
 
-    # Define your prompts here
-    prompts = ["cup"]
+        image = Image.open(BytesIO(image_bytes)).resize((350, 350))
 
-    # Process the image and prompts
-    inputs = processor(text=prompts, images=[image] * len(prompts), padding="max_length", return_tensors="pt")
+        prompts = ["cup"]
+        inputs = self.processor(text=prompts, images=[
+                                image] * len(prompts), padding="max_length", return_tensors="pt")
+        # Model prediction
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+        preds = torch.sigmoid(outputs.logits).numpy()
+        return preds
 
-    # Model prediction
-    with torch.no_grad():
-        outputs = model(**inputs)
-    preds = torch.sigmoid(outputs.logits)
 
-    contours = detect_contours(preds.numpy())
+@stub.function(image=image)
+@wsgi_app()
+def flask_app():
+    from flask import Flask, request, jsonify
 
-    # Create a response object
-    response = SegmentationResponse(contours=contours)
+    web_app = Flask(__name__)
 
-    return jsonify(response.__dict__)
+    @web_app.get("/")
+    def hi():
+        return "hi!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    @web_app.post("/segment")
+    def segment():
+        if 'image' not in request.files:
+            return jsonify({'error': 'No image provided'}), 400
+        image_file = request.files['image']
+        image_bytes = image_file.read()
+        preds = Model().segment.remote(image_bytes)
+
+        # turn to list
+        preds_list = preds.tolist()
+
+        return jsonify(preds_list)
+
+    return web_app
