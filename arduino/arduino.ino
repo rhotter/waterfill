45322ba2f4f673c6785a7ba57e2b17a754c0d764
Rishi Kothari <hey@rishi.cx>
2023-12-15 02:24:00 -0500

    arduino updates

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,43 +1,76 @@
 #include <Servo.h>
 
-// set pin numbers:
-const int SERVO_X_PIN = 10;
-const int SERVO_Y_PIN = 9;
-const int TRIGGER_PIN = 11;
+/*
 
-Servo servoX;  // Servo for LEFT and RIGHT
-Servo servoY;  // Servo for UP and DOWN
+servo x setup
 
-int posX = 90; // Initial position for servoY
-int posY = 90; // Initial position for servoX
+*/
+const int SERVO_X_PIN = 6;
+const int SERVO_X_MAX = 180;
+const int SERVO_X_MIN = 90;
 
-void setup() {
-  servoY.attach(SERVO_X_PIN); // Attach servoY to pin 9
-  servoX.attach(SERVO_Y_PIN); // Attach servoX to pin 10
-  servoY.write(posX); // Set initial position for servoY
-  servoX.write(posY); // Set initial position for servoX
+/*
+
+servo y setup
+
+*/
+const int SERVO_Y_PIN = 5;
+const int SERVO_Y_MAX = 30;
+const int SERVO_Y_MIN = 0;
+
+const int TRIGGER_PIN = 9;
+
+const int increment = 1;
+
+Servo servoX; // Servo for LEFT and RIGHT
+Servo servoY; // Servo for UP and DOWN
+
+int posX = 180; // Initial position for servoY
+int posY = 0;   // Initial position for servoX
+
+void setup()
+{
+  servoY.attach(SERVO_Y_PIN); // Attach servoY to pin 9
+  servoX.attach(SERVO_X_PIN); // Attach servoX to pin 10
+  servoY.write(posY);         // Set initial position for servoY
+  servoX.write(posX);         // Set initial position for servoX
+  pinMode(TRIGGER_PIN, OUTPUT);
   Serial.begin(9600); // Start serial communication
 }
 
-void updateServo(Servo &servo, int &position, int change) {
-  position += change;
-  position = max(0, min(180, position)); // Constrain position between 0 and 180
-  servo.write(position);
+int clamp(int v, int imin, int imax)
+{
+  return max(imin, min(imax, v));
 }
 
-void loop() {
-  if (Serial.available() > 0) {
+void loop()
+{
+  if (Serial.available() > 0)
+  {
     String command = Serial.readStringUntil('\n');
 
-    if (command == "UP") {
-      updateServo(servoY, posX, -10); // Decrease position by 10
-    } else if (command == "DOWN") {
-      updateServo(servoY, posX, 10); // Increase position by 10
-    } else if (command == "LEFT") {
-      updateServo(servoX, posY, -10); // Decrease position by 10
-    } else if (command == "RIGHT") {
-      updateServo(servoX, posY, 10); // Increase position by 10
-    } else if (command.startsWith("SHOOT")) {
+    if (command == "UP")
+    {
+      posY = clamp(posY - increment, SERVO_Y_MIN, SERVO_Y_MAX);
+      servoY.write(posY);
+    }
+    else if (command == "DOWN")
+    {
+      posY = clamp(posY + increment, SERVO_Y_MIN, SERVO_Y_MAX);
+      servoY.write(posY);
+    }
+    else if (command == "LEFT")
+    {
+      posX = clamp(posX + increment, SERVO_X_MIN, SERVO_X_MAX);
+      servoX.write(posX);
+    }
+    else if (command == "RIGHT")
+    {
+      posX = clamp(posX - increment, SERVO_X_MIN, SERVO_X_MAX);
+      servoX.write(posX);
+    }
+    else if (command.startsWith("SHOOT"))
+    {
       int duration = command.substring(6).toInt();
       digitalWrite(TRIGGER_PIN, HIGH); // Assuming trigger is connected to pin TRIGGER_PIN
       delay(duration);
