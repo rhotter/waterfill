71c9a896783916969af1175ce177cfd9ffa2e6c8
Raffi Hotter <raphael.hotter@gmail.com>
2023-12-20 19:05:28 -0500

    Use Owl V2

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ image = (
         "torch",
         "transformers",
         "timm",
+        "scipy",
         "flask"
     ))
 
@@ -16,13 +17,13 @@ stub = Stub("waternug", image=image)
 @stub.cls(gpu="A10G")
 class Model:
     def __enter__(self):
-        from transformers import OwlViTProcessor, OwlViTForObjectDetection
-
+        from transformers import Owlv2Processor, Owlv2ForObjectDetection
         # Load the model and processor
-        self.model = OwlViTForObjectDetection.from_pretrained(
-            "google/owlvit-base-patch32")
-        self.processor = OwlViTProcessor.from_pretrained(
-            "google/owlvit-base-patch32")
+
+        self.processor = Owlv2Processor.from_pretrained(
+            "google/owlv2-base-patch16-ensemble")
+        self.model = Owlv2ForObjectDetection.from_pretrained(
+            "google/owlv2-base-patch16-ensemble")
 
     @method()
     def segment(self, image_bytes):
@@ -46,18 +47,29 @@ class Model:
         with torch.no_grad():
             outputs = model(**inputs)
 
-        target_sizes = torch.Tensor([image.size[::-1]]).to(device)
+        model_image_size = model.config.vision_config.image_size
+        target_sizes = torch.Tensor(
+            [(model_image_size, model_image_size)]).to(device)
         results = self.processor.post_process_object_detection(
-            outputs, threshold=0.5, target_sizes=target_sizes)
+            outputs, threshold=0.4, target_sizes=target_sizes)
 
         results = results[0]
 
         # turn results into a list
-        results_list = [{
-            "score": float(score),
-            "box": (box / model.config.vision_config.image_size).cpu().tolist(),
-            "label": queries[label]
-        } for score, label, box in zip(results["scores"], results["labels"], results["boxes"])]
+        results_list = []
+        aspect_ratio = image.size[0]/image.size[1]
+        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+            x1, y1, x2, y2 = box.cpu().tolist()
+            x1 *= 1/model_image_size
+            x2 *= 1/model_image_size
+            y1 *= aspect_ratio/model_image_size
+            y2 *= aspect_ratio/model_image_size
+
+            results_list.append({
+                "score": float(score),
+                "box": [x1, y1, x2, y2],
+                "label": queries[label]
+            })
 
         return results_list
 
